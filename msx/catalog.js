c5ddd12b46016098857d30d15101f3434ebac15c
cash94 <artemhasan@gmail.com>
2026-03-27 15:34:31 +0300

    Update catalog.js

--- msx/catalog.js
+++ msx/catalog.js
@@ -2133,7 +2133,8 @@ function createCatalogFolderCard(key, config) {
     else if (key.includes('quadhd')) posterHtml = '<img src="https://cash94.github.io/msx/img/Films4k.jpg" style="width: 100%; height: 100%; object-fit: cover;">';
     else if (key.includes('legends')) posterHtml = '<img src="https://cash94.github.io/msx/img/BestFilms.jpg" style="width: 100%; height: 100%; object-fit: cover;">';
     else if (key.includes('tv')) posterHtml = '<img src="https://cash94.github.io/msx/img/Serials.jpg" style="width: 100%; height: 100%; object-fit: cover;">';
-    else if (key.includes('cartoons')) posterHtml = '<img src="https://cash94.github.io/msx/img/Anime.jpg" style="width: 100%; height: 100%; object-fit: cover;">';
+    else if (key.includes('cartoons_tv')) posterHtml = '<img src="https://cash94.github.io/msx/img/multserials.jpg" style="width: 100%; height: 100%; object-fit: cover;">';
+    else if (key.includes('cartoons')) posterHtml = '<img src="https://cash94.github.io/msx/img/multfilms.jpg" style="width: 100%; height: 100%; object-fit: cover;">';
     else if (key.includes('anime')) posterHtml = '<img src="https://cash94.github.io/msx/img/Anime.jpg" style="width: 100%; height: 100%; object-fit: cover;">';
     else posterHtml = '<div style="font-size: 64px; display: flex; align-items: center; justify-content: center; height: 100%;"></div>';
 
