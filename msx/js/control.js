1230fce8ec7fe74e43f2fe133b73f53709dd6c05
cash94 <artemhasan@gmail.com>
2026-04-02 11:03:45 +0300

    Update control.js

--- msx/js/control.js
+++ msx/js/control.js
@@ -1738,7 +1738,7 @@ function setupFocusRescue() {
         return all;
     }
     function getConfigItems() {
-        var ids = ['torrserver-url', 'auth-checkbox', 'auth-login', 'auth-password', '.settings-btn', '#auto-fullscreen'];
+        var ids = ['torrserver-url', 'auth-checkbox', 'auth-login', 'auth-password', '.settings-btn', 'auto-fullscreen'];
         var visible = [];
         for (var i = 0; i < ids.length; i++) {
             var el = byId(ids[i]);
@@ -1789,7 +1789,7 @@ function setupFocusRescue() {
         if (screen === 'config') {
             return !!(
                 el.closest('#config-screen') ||
-                ['torrserver-url', 'auth-checkbox', 'auth-login', 'auth-password'].indexOf(el.id) !== -1 ||
+                ['torrserver-url', 'auth-checkbox', 'auth-login', 'auth-password', 'auto-fullscreen'].indexOf(el.id) !== -1 ||
                 el.classList.contains('settings-btn')
             );
         }
