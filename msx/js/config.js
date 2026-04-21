63308b7a6a043cfd2485dd3a4282209231d51f30
cash94 <artemhasan@gmail.com>
2026-04-21 17:06:20 +0300

    Update config.js

--- msx/js/config.js
+++ msx/js/config.js
@@ -5,7 +5,7 @@ var SERVER_URL = window.location.origin;
 var AppState = {
   // Настройки сервера
   currentTorrserverUrl: '',
-  currentVersion: 'TorrStream.002',
+  currentVersion: 'TorrStream.1.0.3',
   authEnabled: false,
   serverOnline: false,
   clientId: null,
