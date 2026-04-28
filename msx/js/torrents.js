112c8e26712b0a5cc8cd9f5ab3faffff37f24337
cash94 <artemhasan@gmail.com>
2026-04-28 17:18:56 +0300

    Update torrents.js

--- msx/js/torrents.js
+++ msx/js/torrents.js
@@ -2049,7 +2049,7 @@ async function addTorrentToServer(magnet, hash, searchResult) {
     var requestBody = {
       action: 'add',
       link: magnet,
-      searchResult.name+' S['+searchResult.seasons[0]+']',
+      searchResult.name+' S'+searchResult.seasons[0],
       save_to_db: AppState.addToDbEnabled
     };
 
