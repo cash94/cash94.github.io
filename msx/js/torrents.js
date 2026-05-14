6772b40886aa700bc8d9ba138c8edd12f11e00f7
cash94 <artemhasan@gmail.com>
2026-05-14 15:23:31 +0300

    Update torrents.js

--- msx/js/torrents.js
+++ msx/js/torrents.js
@@ -1650,12 +1650,6 @@ async function showDetail(torrent) {
       }
 
       filesList.appendChild(fragment);
-      // Анимация файлов после загрузки
-      if (typeof Animations !== 'undefined') {
-        setTimeout(function () {
-          Animations.animateFilesList();
-        }, 100);
-      }
 
       // Ждем TMDB данные в фоне и обновляем постеры когда они придут
       tmdbPromise.then(function (tmdbData) {
