56e8e849a503ae55edf70736b98e6fdcfe601912
cash94 <artemhasan@gmail.com>
2026-05-16 21:50:09 +0300

    Update catalog.js

--- msx/js/catalog.js
+++ msx/js/catalog.js
@@ -635,7 +635,7 @@ async function loadCatalog(catalogKey) {
     catalogState.posterLoadQueue = [];
     AppState.mediaType = config.mediaType;
 
-    showCatalogLoading('Загрузка ' + config.name + '...');
+    CatalogLoading('Загрузка ' + config.name + '...');
 
     if (catalogCache.has(catalogKey)) {
         var cached = catalogCache.get(catalogKey);
@@ -1672,6 +1672,9 @@ async function loadCatalogPoster(card, title, mediaType, tmdbId, index) {
 // ==================== ДЕТАЛЬНЫЙ ПРОСМОТР ====================
 
 async function showCatalogDetail(item, index, posterUrl) {
+    
+    window.initHorizontalScroll();
+    
     if (posterUrl === undefined) posterUrl = null;
 
     catalogState.lastSelectedIndex = index;
