76f5474a18f03ea3e1fb978c08c1476d37f828f6
cash94 <artemhasan@gmail.com>
2026-05-31 14:01:45 +0300

    Update player.js

--- msx/js/player.js
+++ msx/js/player.js
@@ -1853,7 +1853,9 @@ async function startHLSPlayback(originalUrl, initialSeek, fromSearch, episodeInd
 
       AppState.hls.loadSource(data.playlistUrl);
       AppState.hls.attachMedia(videoPlayer);
-      updatePlayPauseButton();
+      setTimeout(function() {
+        updatePlayPauseButton();
+      }, 20000); // 20000 миллисекунд = 20 секунд
 
     } else if (videoPlayer.canPlayType('application/vnd.apple.mpegurl')) {
       videoPlayer.src = data.playlistUrl;
