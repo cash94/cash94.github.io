0d64866daafbba5a3b65968db5f100f656b7a7a1
cash94 <artemhasan@gmail.com>
2026-06-04 14:34:48 +0300

    Update config.js

--- msx/js/config.js
+++ msx/js/config.js
@@ -68,7 +68,7 @@ var AppState = {
   isSerials: false
 };
 
-var noCacheElements = ['load-more-trigger', 'detail-progress', 'detail-view'];
+var noCacheElements = ['load-more-trigger', 'detail-progress'];
 var domCache = {};
 
 // Кэш для часто используемых DOM-элементов (ленивая инициализация)
