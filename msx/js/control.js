9ab88f33f62a2fecfd6d33b82a76e0735d1dc53f
cash94 <artemhasan@gmail.com>
2026-06-04 08:50:07 +0300

    Update control.js

--- msx/js/control.js
+++ msx/js/control.js
@@ -733,13 +733,14 @@ function focusEl(el, opts) {
 
     return true;
 }
-
+function byId(id) { return getEl(id); };
+function clickEl(el) { try { if (el && el.click) el.click(); } catch (e) { } };
 // ==================== TV FOCUS RESCUE (Оптимизировано) ====================
 function setupFocusRescue() {
     //var VISIBLE = function (el) { return !!(el && el.offsetParent !== null && !el.disabled); };
-    var byId = function (id) { return getEl(id); };
+    //var byId = function (id) { return getEl(id); };
     //var clearFocused = function () { var f = document.querySelectorAll('.focused'); for (var i = 0; i < f.length; i++) { if (typeof gsap !== 'undefined') gsap.killTweensOf(f[i]); f[i].style.boxShadow = ''; f[i].style.transform = ''; f[i].style.scale = ''; f[i].style.translate = ''; f[i].classList.remove('focused'); } };
-    var clickEl = function (el) { try { if (el && el.click) el.click(); } catch (e) { } };
+    //var clickEl = function (el) { try { if (el && el.click) el.click(); } catch (e) { } };
     //var blurEditor = function () { var a = document.activeElement; if (a && a !== document.body && (a.tagName === 'INPUT' || a.tagName === 'SELECT' || a.tagName === 'TEXTAREA')) try { a.blur(); } catch (e) { } };
 
 
