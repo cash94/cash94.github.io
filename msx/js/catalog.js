87e4299a220a84fe4f02392b4d7e186c81ef7b54
cash94 <artemhasan@gmail.com>
2026-06-06 22:18:20 +0300

    Update catalog.js

--- msx/js/catalog.js
+++ msx/js/catalog.js
@@ -1074,3 +1074,4 @@ if (document.readyState === 'loading') document.addEventListener('DOMContentLoad
 
 window.loadCatalogList = showCatalogList; window.backToCatalogList = backToCatalogList; window.exitYoutubePlayer = exitYoutubePlayer; window.loadMoreCatalogItems = loadMoreCatalogItems;
 window.catalog = { loadCatalog: loadCatalog, showCatalogList: showCatalogList, backToCatalogList: backToCatalogList, tmdbCache: { clear: clearTmdbCache, stats: getTmdbCacheStats } };
+window.showCatalogDetail = showCatalogDetail;
