6f79d93158a9b57029b9b09c48d0fbb6df495a29
cash94 <artemhasan@gmail.com>
2026-07-28 09:55:58 +0300

    Update torrents.js

--- public/js/torrents.js
+++ public/js/torrents.js
@@ -603,6 +603,20 @@ function hideCatalogDetailExtra() {
         'catalog-detail-screenshots-wrap', 'catalog-detail-screenshots'
     ];
 
+    var detailProgressBtn = getEl('detail-progress-btn');
+    if (detailProgressBtn) {
+        detailProgressBtn.classList.add('hidden');
+        detailProgressBtn.classList.remove('has-progress');
+        detailProgressBtn.dataset.hash = '';
+        detailProgressBtn.dataset.fileId = '1';
+        detailProgressBtn.dataset.timecode = '0';
+        detailProgressBtn.dataset.episodeIndex = '0';
+        detailProgressBtn.innerHTML = '<span class="btn-label">▶ Играть</span>';
+    }
+    // Кнопку «Играть» из каталога возвращаем
+    var catalogWatchBtn = getEl('catalog-watch-btn');
+    if (catalogWatchBtn) catalogWatchBtn.classList.remove('hidden');
+
     ids.forEach(function (id) {
         var el = getEl(id);
         if (el) {
