d63c8ce8b0f86b93424c3650ad3a5bd2730a8fa0
cash94 <artemhasan@gmail.com>
2026-07-31 10:06:28 +0300

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -578,7 +578,7 @@ function setupNavigation() {
           var searchOverlay = getEl('search-overlay');
           if (searchOverlay) searchOverlay.classList.remove('hidden');
           AppState.currentScreen = 'search';
-          AppState.inSearch = 'search';
+          AppState.isSearch = true;
           AppState.searchResultsHidden = false;
         }
       }
