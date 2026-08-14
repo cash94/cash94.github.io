dc7663ee7eb220a5b222fedc7e696ca529ce2131
cash94 <artemhasan@gmail.com>
2026-08-14 09:12:54 +0300

    Update catalog.js

--- msx/js/catalog.js
+++ msx/js/catalog.js
@@ -6,7 +6,7 @@ var CATALOG_CONSTANTS = {
     CACHE_TTL_MS: 3600000,              // 1 час
     FETCH_TIMEOUT_MS: 5000,             // 5 секунд
     CATALOG_CACHE_TTL_MS: 3600000,      // 1 час для каталогов
-    ITEMS_PER_PAGE: 100,
+    ITEMS_PER_PAGE: 200,
     MAX_POSTER_CACHE: 300,
     MAX_DETAIL_HISTORY: 50,
     POSTER_BATCH_SIZE: 10,
